abcd4e216ef9c05e61e12b42f825a69e1d9bc86f
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-05-28 21:15:00 +0200

    ajout appel a nav service

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\service\nav\MainNavService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $mainNavService;
+
+    public function __construct(MainNavService $mainNavService)
+    {
+        $this->mainNavService = $mainNavService;
+    }
+
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $nav = $this->mainNavService->findAll();
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'nav' => $nav
         ]);
     }
 }
