08bb0577a3d8a40d106bab9769e1127c6e084602
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-05-28 21:15:31 +0200

    ajout path

--- src/Entity/MainNav.php
+++ src/Entity/MainNav.php
@@ -19,6 +19,9 @@ class MainNav
     #[ORM\Column(type: 'boolean')]
     private $dropdown;
 
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $path;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +47,18 @@ class MainNav
     public function setDropdown(bool $dropdown): self
     {
         $this->dropdown = $dropdown;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(?string $path): self
+    {
+        $this->path = $path;
 
         return $this;
     }
