1a9562f17bf5a0c6f421e8e14c72c49267d18caf
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-05-30 18:00:35 +0200

    ajout page produit detail

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\service\nav\MainNavService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProduitController extends AbstractController
+{
+    private MainNavService $mainNavService;
+
+    public function __construct(MainNavService $mainNavService)
+    {
+        $this->mainNavService = $mainNavService;
+    }
+    #[Route('/produit/detail', name: 'app_produit_detail')]
+    public function index(): Response
+    {
+        $nav = $this->mainNavService->findAll();
+        return $this->render('produit/index.html.twig', [
+            'controller_name' => 'ProduitController',
+            'nav' => $nav
+        ]);
+    }
+}
