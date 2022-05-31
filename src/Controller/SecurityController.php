19c3d91269633eec38b31b650717c7c482966850
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-05-31 12:26:32 +0200

    ajout login et register, ajout user entité

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\service\nav\MainNavService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    private MainNavService $mainNavService;
+
+    public function __construct(MainNavService $mainNavService)
+    {
+        $this->mainNavService = $mainNavService;
+    }
+    
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        //pour afficher la nav
+        $nav = $this->mainNavService->findAll();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error,
+            'nav' => $nav
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
