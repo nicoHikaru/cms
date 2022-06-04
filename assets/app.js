bb54f05ad63480d051fbaf350754bc227bbee1c2
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-06-04 14:38:27 +0200

    ajout modal connexion et upadate scss

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,6 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import './js/favoris.js';
+import './js/modal.js';
---
+++ assets/js/favoris.js
@@ -0,0 +1,50 @@
+let favoris = document.querySelector('.favoris');
+let produitId = document.getElementById('produitId');
+let userId = document.getElementById('userId');
+let fav = document.getElementById('fav');
+
+favoris.addEventListener('click',(event) => {
+   event.preventDefault();
+   
+   async function getAuteurs(){
+      if(userId.value !== "none") {
+         let rep = await fetch('/produit/favoris/' + produitId.value + '/' + userId.value , { method: 'GET' });
+         let reponse = await rep.json();
+
+         if(reponse.favoris === true) {
+            favoris.removeChild(fav);
+            let svg = `
+               <svg xmlns="http://www.w3.org/2000/svg" id="fav" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+               </svg>
+            `;
+            favoris.innerHTML = svg;
+         }
+         if(reponse.favoris === false) {
+            favoris.removeChild(fav);
+            let svg = `
+               <svg xmlns="http://www.w3.org/2000/svg" id="fav" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+               </svg>
+            `;
+            favoris.innerHTML = svg ;
+         }
+         console.log(reponse);
+         //return reponse;
+      }
+      
+  }
+  getAuteurs();
+});
+
+//  // crée un nouvel élément div
+//  var newSvg = document.createElement("svg");
+//  var newPath = document.createElement("path");
+//  // et lui donne un peu de contenu
+//  var newContent = document.createTextNode('Hi there and greetings!');
+//  // ajoute le nœud texte au nouveau div créé
+//  newSvg.appendChild(newPath);
+
+//  // ajoute le nouvel élément créé et son contenu dans le DOM
+//  var currentDiv = document.getElementById('div1');
+//  document.body.insertBefore(newDiv, currentDiv);
