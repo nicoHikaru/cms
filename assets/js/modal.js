2fe6f57bc5b1a6a86c25bb7cfee13ff9fff1e419
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-06-04 15:46:53 +0200

    favoris fonctionne

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -8,13 +8,15 @@ var btn = document.getElementById("favoris");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+if(btn !== null) {
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+    modal.style.display = "none";
+  
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
