44c704ba231d4b333d5b279adc3dfdc85f3d70e4
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-06-13 14:19:09 +0200

    update scss et js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,8 +1,9 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("favoris");
+let modal = document.getElementById("myModal");
+if(modal !== null) {
+  // Get the button that opens the modal
+let btn = document.getElementById("favoris");
+let btnsHome = document.querySelectorAll('.modalCarts');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -19,9 +20,24 @@ if(btn !== null) {
     }
 }
 
+if(btnsHome !== null) {
+  btnsHome.forEach((btn) => {
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+  })
+  
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+    modal.style.display = "none";
+  
+    }
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
 }
