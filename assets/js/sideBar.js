09278ea2b692ec38344aa5f1dc20188edee78666
queirolo nicolas <7379368-nicoHikaru@users.noreply.gitlab.com>
2022-06-21 15:14:43 +0200

    correction side bar position fixed

--- assets/js/sideBar.js
+++ assets/js/sideBar.js
@@ -7,7 +7,7 @@ let sideBarArrowRight = document.getElementById('sideBarArrowRight');
 
 if(sideBar !== null) {
     sideBarArrow.addEventListener('click', () => {
-        sideBar.style.width = "5%";
+        sideBar.style.width = "6%";
         sideBar.style.transition = "1s";
         liste.style.display = "none";
         liste.style.transition = "0.5s";
@@ -23,3 +23,15 @@ if(sideBar !== null) {
         listeSideBar.style.display = "none";
     });
 }
+
+// window.onscroll = function() {myFunction()};
+
+// let sticky = sideBar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     sideBar.style.position = "fixed";
+//   } else {
+//     sideBar.style.position = "absolute";
+//   }
+// }
